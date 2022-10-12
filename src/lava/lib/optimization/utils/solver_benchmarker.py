f934737bf2130eec731528c9160ee7b86b50ecaf
CHINONSO ONAH <chinonso.onah@intel.com>
2022-10-12 07:05:55 -0700

    Revert "Added a database to SolverBenchmarker to generate results."
    This reverts commit 1f95f76dcc8d04b01ab1986ce5204656d389829e.

--- src/lava/lib/optimization/utils/solver_benchmarker.py
+++ src/lava/lib/optimization/utils/solver_benchmarker.py
@@ -9,9 +9,4 @@ class SolverBenchmarker:
 
 class SolverBenchmarkerDatabase:
     """Creates a database for energy and time measurements."""
-    def benchmark_Power_measurements():
-        pass
     pass
-
-avg_time_per_timestep
-total_power
