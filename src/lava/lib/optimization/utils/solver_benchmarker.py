477f27b5910c5a08641d2b8638278a69e0082833
CHINONSO ONAH <chinonso.onah@intel.com>
2022-10-12 02:11:46 -0700

    create SolverBenchmarker class
    Signed-off-by: CHINONSO ONAH <chinonso.onah@intel.com>

---
+++ src/lava/lib/optimization/utils/solver_benchmarker.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2021 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+
+
+class SolverBenchmarker:
+    """Measure power and execution time for an optimization solver."""
+    pass
